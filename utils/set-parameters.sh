615f74a9e41478654295d56a9d607f7ffe98056c
Encora - Johan Rojas <encora-johan.rojas@mibanco.com.pe>
2025-03-17 14:56:50 -0500

    update

---
+++ utils/set-parameters.sh
@@ -0,0 +1,10 @@
+echo "NOMBRE=$(echo $componente | jq -r '.nombre')" >> $GITHUB_ENV
+echo "NAMESPACE=$(echo $componente | jq -r '.namespace')" >> $GITHUB_ENV
+echo "AKS=$(echo $componente | jq -r '.aks')" >> $GITHUB_ENV
+echo "SQUAD=$(echo $componente | jq -r '.squad')" >> $GITHUB_ENV
+echo "REPO_VALIDATION=$(echo $componente | jq -r '.repoValidation')" >> $GITHUB_ENV
+echo "FOLDER_VALIDATION=$(echo $componente | jq -r '.folderValidation')" >> $GITHUB_ENV
+echo "SMOKE_FILE=$(echo $componente | jq -r '.smokeFile')" >> $GITHUB_ENV
+echo "ACCEPTANCE_FILE=$(echo $componente | jq -r '.acceptanceFile')" >> $GITHUB_ENV
+echo "NO_AG=$(echo $componente | jq -r '.noAG')" >> $GITHUB_ENV
+echo "SERVICE_PORT=$(echo $componente | jq -r '.servicePort')" >> $GITHUB_ENV
